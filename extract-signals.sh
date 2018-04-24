5ec2297cf9257db29f79508f1c4c235d2a7dc440
Luigi Selmi <luigiselmi@gmail.com>
2018-04-24 14:06:12 +0200

    minor changes

--- extract-signals.sh
+++ extract-signals.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar target/mahoutdataproducer-0.0.1-SNAPSHOT-jar-with-dependencies.jar -output signals.csv -comparisons ../user-navigation/cortex01/pre-lf01/comparisons -views ../user-navigation/cortex01/pre-lf01/views -downloads ../user-navigation/cortex01/pre-lf01/downloads 
+java -jar target/mahoutdataproducer-0.1.0-SNAPSHOT-jar-with-dependencies.jar -output signals.csv -comparisons ../user-navigation/cortex01/pre-lf01/comparisons -views ../user-navigation/cortex01/pre-lf01/views -downloads ../user-navigation/cortex01/pre-lf01/downloads 
